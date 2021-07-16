a33a9a74a4f5c1ced99dc63306ae714f7df5039c
shivaduke28 <shivaduke28>
2021-07-16 23:30:45 +0900

    OnValidateでtagを切り替えるのをやめる

--- VRCWorldPublic/Assets/Kanikama/Scripts/EditorOnly/EditorOnlyBehaviour.cs
+++ VRCWorldPublic/Assets/Kanikama/Scripts/EditorOnly/EditorOnlyBehaviour.cs
@@ -4,9 +4,5 @@ namespace Kanikama.EditorOnly
 {
     public class EditorOnlyBehaviour : MonoBehaviour
     {
-        private void OnValidate()
-        {
-            gameObject.tag = "EditorOnly";
-        }
     }
 }
