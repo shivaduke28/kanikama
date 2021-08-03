c289f9de8cdc6c9e793b8be0f2e6a7d8e1795176
shivaduke28 <shivaduke28>
2021-08-04 01:41:13 +0900

    using 削除

--- Kanikama/Assets/Kanikama/Scripts/Editor/Baker.cs
+++ Kanikama/Assets/Kanikama/Scripts/Editor/Baker.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
