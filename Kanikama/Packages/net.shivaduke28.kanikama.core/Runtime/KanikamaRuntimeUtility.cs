ad6bfe859246affa3e164c902862beee83b89099
shivaduke28 <shivaduke28@gmail.com>
2023-04-15 00:43:42 +0900

    material instance holder

--- Kanikama/Packages/net.shivaduke28.kanikama.core/Runtime/KanikamaRuntimeUtility.cs
+++ Kanikama/Packages/net.shivaduke28.kanikama.core/Runtime/KanikamaRuntimeUtility.cs
@@ -2,8 +2,27 @@
 
 namespace Kanikama.Core
 {
-    public static class MaterialUtility
+    public static class KanikamaRuntimeUtility
     {
+        public static void DestroySafe(Object obj)
+        {
+            if (obj == null) return;
+#if UNITY_EDITOR
+            Object.DestroyImmediate(obj);
+#else
+            Object.Destroy(obj);
+#endif
+        }
+
+        public static T GetOrAddComponent<T>(GameObject gameObject) where T : Component
+        {
+            if (!gameObject.TryGetComponent<T>(out var component))
+            {
+                component = gameObject.AddComponent<T>();
+            }
+            return component;
+        }
+
         public static bool IsContributeGI(Material material)
         {
             return material.globalIlluminationFlags.HasFlag(MaterialGlobalIlluminationFlags.BakedEmissive);
