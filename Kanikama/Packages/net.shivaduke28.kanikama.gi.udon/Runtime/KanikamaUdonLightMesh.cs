f842460e1c823539a5985720be4b515fc583bd35
shivaduke28 <shivaduke28@gmail.com>
2023-05-28 13:56:55 +0900

    kanikama udon light mesh

---
+++ Kanikama/Packages/net.shivaduke28.kanikama.gi.udon/Runtime/KanikamaUdonLightMesh.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using VRC.SDKBase;
+
+namespace Kanikama.GI.Udon
+{
+    [RequireComponent(typeof(Renderer))]
+    public class KanikamaUdonLightMesh : KanikamaUdonLightSource
+    {
+        [SerializeField] new Renderer renderer;
+        [SerializeField] int materialIndex;
+        [SerializeField] string propertyName = "_EmissionColor";
+        [SerializeField] Material instance;
+        [SerializeField] bool useMaterialPropertyBlock = true;
+
+        bool useMaterialPropertyBlockInternal;
+
+        int propertyId;
+        MaterialPropertyBlock block;
+
+        void Awake()
+        {
+            propertyId = VRCShader.PropertyToID(propertyName);
+            useMaterialPropertyBlockInternal = useMaterialPropertyBlock;
+            if (useMaterialPropertyBlockInternal)
+            {
+                block = new MaterialPropertyBlock();
+            }
+            else
+            {
+                instance = renderer.materials[materialIndex];
+            }
+        }
+
+        void OnValidate()
+        {
+            renderer = GetComponent<Renderer>();
+        }
+
+        public override Color GetLinearColor()
+        {
+            if (useMaterialPropertyBlockInternal)
+            {
+                renderer.GetPropertyBlock(block);
+                return block.GetColor(propertyId).linear;
+            }
+            else
+            {
+                return instance.GetColor(propertyId).linear;
+            }
+        }
+
+        void OnDestroy()
+        {
+            if (instance != null)
+            {
+                Destroy(instance);
+            }
+        }
+    }
+}
