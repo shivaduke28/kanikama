cac99ee1f2e0a8028696bec10f06a9850613d695
shivaduke28 <shivaduke28@gmail.com>
2023-05-28 13:16:38 +0900

    add missing rename

--- Kanikama/Packages/net.shivaduke28.kanikama.gi/Runtime/Impl/KanikamaRuntimeLightMesh.cs
+++ Kanikama/Packages/net.shivaduke28.kanikama.gi/Runtime/Impl/KanikamaRuntimeLightMesh.cs
@@ -3,7 +3,7 @@
 namespace Kanikama.GI.Runtime.Impl
 {
     [RequireComponent(typeof(Renderer))]
-    public sealed class KanikamaRuntimeLightMaterial : LightSource
+    public sealed class KanikamaRuntimeLightMesh : LightSource
     {
         [SerializeField] new Renderer renderer;
         [SerializeField] int materialIndex;
