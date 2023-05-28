f82f285e4408e65660f7922d5a69374c4955244d
shivaduke28 <shivaduke28@gmail.com>
2023-05-28 13:23:27 +0900

    add UdonSceneDesciptor

---
+++ Kanikama/Packages/net.shivaduke28.kanikama.gi.udon/Baking/KanikamaUdonSceneDescriptor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Kanikama.GI.Baking;
+using Kanikama.GI.Baking.Impl;
+using UnityEngine;
+
+namespace Kanikama.GI.Udon.Baking
+{
+    public sealed class KanikamaUdonSceneDescriptor : MonoBehaviour, IBakingDescriptor
+    {
+        [SerializeField] KanikamaBakeTargetLight[] kanikamaBakeTargetLights;
+        [SerializeField] KanikamaBakeTargetLightMesh[] kanikamaBakeTargetLightMeshes;
+        [SerializeField] KanikamaBakeTargetMonitorGroup[] kanikamaBakeTargetMonitorGroups;
+
+        // Used only to setup ColorCollector in Editor
+        public Light[] GetLights() => kanikamaBakeTargetLights.Select(x => x.GetComponent<Light>()).ToArray();
+
+        public Renderer[] GetRenderers() => kanikamaBakeTargetLights.Select(x => x.GetComponent<Renderer>()).ToArray();
+
+        List<BakeTarget> IBakingDescriptor.GetBakeTargets()
+        {
+            var targets = new List<BakeTarget>(kanikamaBakeTargetLights);
+            targets.AddRange(kanikamaBakeTargetLightMeshes);
+            return targets;
+        }
+
+        List<BakeTargetGroup> IBakingDescriptor.GetBakeTargetGroups()
+        {
+            return kanikamaBakeTargetMonitorGroups.Cast<BakeTargetGroup>().ToList();
+        }
+    }
+}
