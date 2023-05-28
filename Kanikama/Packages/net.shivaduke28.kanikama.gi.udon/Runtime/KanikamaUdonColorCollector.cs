4cf1474cd5a1752972692abb38847d3328be394f
shivaduke28 <shivaduke28@gmail.com>
2023-05-28 14:36:17 +0900

    adjust color collector

--- Kanikama/Packages/net.shivaduke28.kanikama.gi.udon/Runtime/KanikamaUdonColorCollector.cs
+++ Kanikama/Packages/net.shivaduke28.kanikama.gi.udon/Runtime/KanikamaUdonColorCollector.cs
@@ -1,5 +1,4 @@
-﻿
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
 
 namespace Kanikama.GI.Udon
@@ -7,13 +6,12 @@ namespace Kanikama.GI.Udon
     [RequireComponent(typeof(Camera)), UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
     public class KanikamaUdonColorCollector : UdonSharpBehaviour
     {
-        [SerializeField] Light[] lights;
-        [SerializeField] Renderer[] emissiveRenderers;
-        [SerializeField] KanikamaUdonMonitorCamera[] kanikamaCameras;
+        [SerializeField] KanikamaUdonLightSource[] lightSources;
+        [SerializeField] KanikamaUdonLightSourceGroup[] lightSourceGroups;
 
-        [Space]
-        [Range(0, 20f)] public float intensity = 1f;
-        [SerializeField, HideInInspector] Vector4[] colors; // linear
+        [SerializeField] Vector4[] colors; // linear
+
+        const int MaxColorCount = 64;
 
         public Vector4[] GetColors()
         {
@@ -25,73 +23,27 @@ namespace Kanikama.GI.Udon
         }
 
         bool isInitialized;
-        int frameCount;
 
-        int size;
-        int lightCount;
-
-        int rendererCount;
-        int[] materialCounts;
-        bool[][] materialEmissiveFlags;
-
-        int monitorCount;
-        Color[][] monitorColors;
-        int[] monitorLightCounts;
+        Color[][] lightSourceGroupColors;
+        int[] lightSourceGroupStartIndex;
 
         void Initialize()
         {
-            size = 0;
+            colors = new Vector4[MaxColorCount];
+            var index = lightSources.Length;
 
-            // Light
-            lightCount = lights == null ? 0 : lights.Length;
-            size += lightCount;
-
-            // Renderer
-            rendererCount = emissiveRenderers == null ? 0 : emissiveRenderers.Length;
-            if (rendererCount > 0)
+            var lightSourceGroupCount = lightSourceGroups.Length;
+            lightSourceGroupColors = new Color[lightSourceGroupCount][];
+            lightSourceGroupStartIndex = new int[lightSourceGroupCount];
+            for (var i = 0; i < lightSourceGroupCount; i++)
             {
-                materialCounts = new int[rendererCount];
-                materialEmissiveFlags = new bool[rendererCount][];
+                var lightSourceGroup = lightSourceGroups[i];
+                var groupColors = lightSourceGroup.GetLinearColors();
+                lightSourceGroupColors[i] = groupColors;
+                lightSourceGroupStartIndex[i] = index;
+                index += groupColors.Length;
             }
 
-            for (var i = 0; i < rendererCount; i++)
-            {
-                var renderer = emissiveRenderers[i];
-                var mats = renderer.sharedMaterials;
-                var count = mats.Length;
-                var flags = new bool[count];
-                materialCounts[i] = count;
-                for (var j = 0; j < count; j++)
-                {
-                    var isEmissive = ((byte)mats[j].globalIlluminationFlags & (byte)MaterialGlobalIlluminationFlags.BakedEmissive) == (byte)MaterialGlobalIlluminationFlags.BakedEmissive;
-                    flags[j] = isEmissive;
-                    if (isEmissive)
-                    {
-                        size++;
-                    }
-                }
-                materialEmissiveFlags[i] = flags;
-            }
-
-            // Monitor
-            monitorCount = kanikamaCameras == null ? 0 : kanikamaCameras.Length;
-            if (monitorCount > 0)
-            {
-                monitorColors = new Color[monitorCount][];
-                monitorLightCounts = new int[monitorCount];
-            }
-
-            for (var i = 0; i < monitorCount; i++)
-            {
-                var kanikamaCamera = kanikamaCameras[i];
-                var cols = kanikamaCamera.GetColors();
-                var colCount = cols.Length;
-                monitorColors[i] = cols;
-                monitorLightCounts[i] = colCount;
-                size += colCount;
-            }
-
-            colors = new Vector4[size];
             isInitialized = true;
         }
 
@@ -100,52 +52,20 @@ namespace Kanikama.GI.Udon
         // KanikamaUdonGIUpdater (and other scripts) attached to the same GameObject should use them on or after OnPreRender.
         void OnPreCull()
         {
-            if (frameCount >= Time.frameCount) return;
-            frameCount = Time.frameCount;
-
-            var index = 0;
-
-            // 1. Light (include Ambient)
-            for (var i = 0; i < lightCount; i++)
+            for (var i = 0; i < lightSources.Length; i++)
             {
-                var light = lights[i];
-                // NOTE: depends on GraphicsSettings.lightsUseLinearIntensity
-                colors[index] = light.color.linear * light.intensity * intensity;
-                index++;
+                colors[i] = lightSources[i].GetLinearColor();
             }
 
-            // 2. Renderer
-            for (var i = 0; i < rendererCount; i++)
+            for (var i = 0; i < lightSourceGroups.Length; i++)
             {
-                var renderer = emissiveRenderers[i];
-                var mats = renderer.materials;
-                var count = materialCounts[i];
-                var emissiveFlags = materialEmissiveFlags[i];
-                for (var j = 0; j < count; j++)
+                var groupColors = lightSourceGroupColors[i];
+                var offset = lightSourceGroupStartIndex[i];
+                for (var j = 0; j < groupColors.Length; j++)
                 {
-                    if (emissiveFlags[j])
-                    {
-                        // HDR (linear)
-                        colors[index] = mats[j].GetColor("_EmissionColor") * intensity;
-                        index++;
-                    }
+                    colors[offset + j] = groupColors[j];
                 }
             }
-
-            // 3. Monitor
-            for (var i = 0; i < monitorCount; i++)
-            {
-                var cols = monitorColors[i];
-                var count = monitorLightCounts[i];
-                for (var j = 0; j < count; j++)
-                {
-                    // monitorColors should be linear
-                    colors[index] = cols[j] * intensity;
-                    index++;
-                }
-            }
-
-            // 4. Others (You can add custom udon scripts here)
         }
     }
 }
