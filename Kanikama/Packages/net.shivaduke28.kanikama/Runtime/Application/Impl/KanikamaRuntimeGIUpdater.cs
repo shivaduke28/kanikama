14ca49b3e3132edd6286a42f35522d63c6204bd3
shivaduke28 <shivaduke28@gmail.com>
2023-06-20 22:44:24 +0900

    merge LTC updater to GI updater

--- Kanikama/Packages/net.shivaduke28.kanikama/Runtime/Application/Impl/KanikamaRuntimeGIUpdater.cs
+++ Kanikama/Packages/net.shivaduke28.kanikama/Runtime/Application/Impl/KanikamaRuntimeGIUpdater.cs
@@ -9,6 +9,7 @@ namespace Kanikama.Application.Impl
     {
         [SerializeField] bool isSRP;
         [SerializeField] Camera targetCamera;
+        [Header("Scene")]
         [SerializeField] List<LightSource> lightSources;
         [SerializeField] List<LightSourceGroup> lightSourceGroups;
         [SerializeField] Renderer[] receivers;
@@ -24,6 +25,29 @@ namespace Kanikama.Application.Impl
         Vector4[] colorsInternal;
         MaterialPropertyBlock block;
         List<IndexedColorArray> indexedColorArrays;
+
+        [Header("LTC")] [SerializeField] Transform[] ltcMonitors;
+        [SerializeField] Texture[] ltcVisibilityMaps;
+        [SerializeField] Texture ltcLut0;
+        [SerializeField] Texture ltcLut1;
+        [SerializeField] Texture ltcLightSourceTex;
+
+        readonly Vector4[] vertex0 = new Vector4[3];
+        readonly Vector4[] vertex1 = new Vector4[3];
+        readonly Vector4[] vertex2 = new Vector4[3];
+        readonly Vector4[] vertex3 = new Vector4[3];
+
+        static readonly int LtcCount = Shader.PropertyToID("_Udon_LTC_Count");
+        static readonly int LtcVisibilityMap = Shader.PropertyToID("_Udon_LTC_VisibilityMap");
+        static readonly int LtcVertex0 = Shader.PropertyToID("_Udon_LTC_Vertex0");
+        static readonly int LtcVertex1 = Shader.PropertyToID("_Udon_LTC_Vertex1");
+        static readonly int LtcVertex2 = Shader.PropertyToID("_Udon_LTC_Vertex2");
+        static readonly int LtcVertex3 = Shader.PropertyToID("_Udon_LTC_Vertex3");
+        static readonly int UdonLtcLut0 = Shader.PropertyToID("_Udon_LTC_LUT0");
+        static readonly int UdonLtcLut1 = Shader.PropertyToID("_Udon_LTC_LUT1");
+        static readonly int UdonLtcLightTex0 = Shader.PropertyToID("_Udon_LTC_LightTex0");
+
+        int ltcMonitorCount;
 
         void OnBeginCameraRendering(ScriptableRenderContext context, Camera camera)
         {
@@ -100,14 +124,32 @@ namespace Kanikama.Application.Impl
                 }
 
                 r.GetPropertyBlock(block);
+
+                // lightmap
                 block.SetTexture(LightmapArray, lightmapArrays[i]);
+                // directional map
                 if (i < directionalLightmapArrays.Length)
                 {
                     block.SetTexture(LightmapIndArray, directionalLightmapArrays[i]);
                 }
+                // ltc visibility
+                if (i < ltcVisibilityMaps.Length)
+                {
+                    block.SetTexture(LtcVisibilityMap, ltcVisibilityMaps[i]);
+                }
+
                 r.SetPropertyBlock(block);
             }
+
             Shader.SetGlobalInt(Count, index);
+
+            ltcMonitorCount = Mathf.Min(3, ltcMonitors.Length);
+            Shader.SetGlobalInt(LtcCount, ltcMonitorCount);
+            Shader.SetGlobalTexture(UdonLtcLut0, ltcLut0);
+            Shader.SetGlobalTexture(UdonLtcLut1, ltcLut1);
+            Shader.SetGlobalTexture(UdonLtcLightTex0, ltcLightSourceTex);
+
+            UpdateLtcVertexPositions();
         }
 
         void UpdateColors()
@@ -129,6 +171,38 @@ namespace Kanikama.Application.Impl
             }
 
             Shader.SetGlobalVectorArray(Colors, colorsInternal);
+        }
+
+        void UpdateLtcVertexPositions()
+        {
+            // Unity's Quad mesh
+            var w = 0.5f;
+            var h = 0.5f;
+
+            for (var i = 0; i < ltcMonitorCount; i++)
+            {
+                var localToWorld = ltcMonitors[i].localToWorldMatrix;
+
+                var p0 = new Vector3(w, -h, 0);
+                var p1 = new Vector3(-w, -h, 0);
+                var p2 = new Vector3(-w, h, 0);
+                var p3 = new Vector3(w, h, 0);
+
+                p0 = localToWorld.MultiplyPoint3x4(p0);
+                p1 = localToWorld.MultiplyPoint3x4(p1);
+                p2 = localToWorld.MultiplyPoint3x4(p2);
+                p3 = localToWorld.MultiplyPoint3x4(p3);
+
+                vertex0[i] = p0;
+                vertex1[i] = p1;
+                vertex2[i] = p2;
+                vertex3[i] = p3;
+            }
+
+            Shader.SetGlobalVectorArray(LtcVertex0, vertex0);
+            Shader.SetGlobalVectorArray(LtcVertex1, vertex1);
+            Shader.SetGlobalVectorArray(LtcVertex2, vertex2);
+            Shader.SetGlobalVectorArray(LtcVertex3, vertex3);
         }
 
         class IndexedColorArray
