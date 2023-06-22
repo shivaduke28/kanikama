4513f35dac356fd55be4867ce67806265d0fa3b3
shivaduke28 <shivaduke28@gmail.com>
2023-06-23 02:04:57 +0900

    not null attribute

---
+++ Kanikama/Packages/net.shivaduke28.kanikama/Runtime/Baking/Attributes/NotNullAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kanikama.Baking.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class NonNullAttribute : PropertyAttribute
+    {
+    }
+}
