5c6eb1e081f1cc6723ee064adacc048c6473e4a8
shivaduke28 <shivaduke28@gmail.com>
2023-07-08 19:37:35 +0900

    add KanikamaUdonAudioLinkLightSource

---
+++ Kanikama/Packages/net.shivaduke28.kanikama.udon/Runtime/AudioLink/KanikamaUdonAudioLinkLightSource.cs
@@ -0,0 +1,33 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace Kanikama.Udon.AudioLink
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public sealed class KanikamaUdonAudioLinkLightSource : KanikamaUdonLightSource
+    {
+        [SerializeField] VRCAudioLink.AudioLink audioLink;
+        [SerializeField] int band;
+        [SerializeField] int delay;
+
+        int dataIndex;
+
+        void Start()
+        {
+            dataIndex = band * 128 + delay;
+        }
+
+        public override Color GetLinearColor()
+        {
+            var audioData = audioLink.audioData;
+            if (audioData.Length != 0)
+            {
+                return audioData[dataIndex].linear;
+            }
+            else
+            {
+                return Color.black;
+            }
+        }
+    }
+}
